cdac78153c14e1460a5db272e33668e3dcc617ea
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2024-10-31 17:31:52 +0300

    feat: add confetti on done

--- src/views/widgets/ListItem/ListItem.tsx
+++ src/views/widgets/ListItem/ListItem.tsx
@@ -1,11 +1,11 @@
-import { ChangeEventHandler, FC, MouseEventHandler, ReactNode, useState } from 'react';
+import { ChangeEventHandler, FC, MouseEventHandler, ReactNode, useCallback, useEffect, useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import AccordionItem from 'react-bootstrap/AccordionItem';
 import AccordionHeader from 'react-bootstrap/AccordionHeader';
 import AccordionBody from 'react-bootstrap/AccordionBody';
 import AccordionButton from 'react-bootstrap/AccordionButton';
 import classNames from 'classnames';
-
+import ConfettiExplosion from 'react-confetti-explosion';
 import styles from './ListItem.module.scss';
 
 interface Props {
@@ -22,9 +22,27 @@ interface Props {
 
 export const ListItem: FC<Props> = (props: Props) => {
   const { children, onDone, id, onDelete, onEdit, complete = false, increasePriority, decreasePriority } = props;
+  const [isCongrats, setIsCongrats] = useState(false);
+  const handleDone = useCallback(
+    (e) => {
+      if (!complete) setIsCongrats(true);
+      onDone(e);
+    },
+    [complete, onDone],
+  );
+  //   useEffect(() => {
+  //     const timer = setTimeout(setIsCongrats, 1000, false);
+  //     return () => clearTimeout(timer);
+  //   }, [isCongrats]);
   return (
     <li className={styles.item}>
-      <input type="checkbox" data-id={id} onChange={onDone} checked={complete} />
+      {isCongrats && (
+        <>
+          <ConfettiExplosion width={800} onComplete={() => setIsCongrats(false)} />
+          <div />
+        </>
+      )}
+      <input type="checkbox" data-id={id} onChange={handleDone} checked={complete} />
       <input
         data-id={id}
         onChange={onEdit}
