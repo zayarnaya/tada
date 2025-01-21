73c5ebd6810b701d7a7d6910c5ffcb39975fbeb5
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2025-01-21 14:31:04 +0300

    feat: apdate vite config and index.html to meet requirements

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'TaDa - the Simple ToDo App',
+        short_name: 'TaDa',
+        description: 'Todo App - add and manage your todos',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{ts,tsx,scss,html,ico,png,svg,jpg,js,css}'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
